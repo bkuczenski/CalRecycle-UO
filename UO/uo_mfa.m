85eb799b10f45000d392f9ad3228e01434a74a5a
Brandon Kuczenski <brandon.kuczenski@301south.net>
2012-11-08 16:20:26 -0800

    Break out separate config file

--- UO/uo_mfa.m
+++ UO/uo_mfa.m
@@ -1,91 +1,9 @@
 %% script file for performing the UO mfa
 %%
-%%=========================================================================
-%%=========================================================================
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%
-%% CONFIGURATION HAPPENS HERE!
-%% 
-%% Make changes to the below lines to tune the file to your operating 
-%% conditions.
-%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% Select which parts of the MFA to run
-YEARS=2007:2011;
-WCs=221:223;
-
-READ_FACILITIES = false;
-READ_NAICS      = false;
-LOAD_MD_NODE    = false;
-GEN_MD          = false;
-UNITCONV_MD     = false;
-RE_CORR_METH    = false;
-GEN_NODE        = false;
-GEN_NODE_FORCE  = false;
-LOAD_CR_PROC    = false;
-GEN_NODE_PIVOT  = false;
-PUBLISH_DATA    = false;
-APPLY_FAC_DATA  = true;
-FORCE_FAC_DATA  = true;
-COMPUTE_ACTIVITY = true;
-
-%% INPUT FILES
-%% Location of facility description data in csv format
-% modified to include DESTINATION_UNKNOWN, SOURCE_UNKNOWN, and custom additions
-FACILITIES_FILE='HWTS_FACILITIES_2007_2011.csv'; 
-NAICS_FILE='HWTS_FACILITIES_NAICS.csv';
-
-FACILITIES_PREFIX='../FacilityData/';
-
-TANNER_PREFIX='../TannerData/'; % path to Tanner report data - each year in 'TannerYYYY'
-                          % subdir - list of manifest files is hardcoded into
-                          % uo_load (based on downloadable data)
-CALRECYCLE_PREFIX='../CalRecycleData/'; %% path to CalRecycle data - need
-                                    % CR-processor.csv
 
 
-%% Management Method Codes we're interested in
-METH_REGEXP='^H[0-9]{3}';  % regexp to match all method codes
-TANNER_DISP='H039';  % net flows into a facility are given this code
-TANNER_TERMINAL={'H010','H020','H040','H050','H061','H081','H111','H129','H132', ...
-                 'H135'}; % these codes are considered "terminal" & do not add to
-                          % the quantity of oil to TANNER_DISP 
-
-% The following codes (1) are valid, (2) show up in the data, and (3) have not
-% been incorporated and are therefore interpreted as H039:
-% H077 (4x), H101 (9x), H103 (2x), H121 (corrected from H221, 1x), H122 (1x), H123
-% (5x), H131 (4x), H134 (1x), totalling 318,000 gallons over 8 years.
-
-% of these, H077, H101, H123 can reasonably be considered to produce RFO and
-% should properly count as H039 (totalling 254,000 gal)
-
-% The rest are just not worth considering.
-
-%% Output Files
-% BK
- FILE_EXCHANGE='../../research.bren/CalRecycle-Oil/Working Documents/Data Collection/MFA/';
-% TZ
-% FILE_EXCHANGE='C:\tzink\Dropbox\School\PhD\Research\Projects & Papers\CalRecycle Used Oil LCA\Working Documents\Data Collection\MFA\';
-NODE_PIVOT_PREFIX='UO_facilities';
-ACTIVITY_FILE_PREFIX='UO_activity';
-
-FAC_DATA_FILE=[FILE_EXCHANGE 'Facilities.xlsx'];
-FAC_DATA_SHEET='Activities';
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%
-%% END CONFIG
-%% 
-%% Only nake changes above this line.
-%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%=========================================================================
-%%=========================================================================
+%% Load configuration
+uo_config
 
 
 NODE_PIVOT_FILE=[NODE_PIVOT_PREFIX '_' num2str(YEARS(1))];
@@ -350,7 +268,7 @@ end
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-if GEN_NODE
+if GEN_NODE | GEN_NODE_FORCE
   fprintf('%s ... %.1f sec\n','Computing Node Mass Balances',toc)
   for i=1:length(YEARS)
     yy=num2str(YEARS(i));
