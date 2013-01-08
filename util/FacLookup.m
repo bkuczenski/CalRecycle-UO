fd470970bdfa8092ce5d87c4266247d243e81547
Brandon Kuczenski <bkuczenski@bren.ucsb.edu>
2013-01-07 16:33:45 -0800

    Vectorize FacLookup

--- util/FacLookup.m
+++ util/FacLookup.m
@@ -1,5 +1,13 @@
 function FacLookup(ID)
 % function FacLookup(ID)
 global Facilities
-Facilities(FACILITY_lookup(ID))
+if iscell(ID)
+  for k=1:length(ID)
+    s(k)=FACILITY_lookup(ID{k});
+  end
+  show(Facilities(s))
+else
+  Facilities(FACILITY_lookup(ID))
+end
+
 
