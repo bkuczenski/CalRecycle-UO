2dcedfceea04db633c8b50e6d1147cd23e5f0fbd
bkuczenski <brandon.kuczenski@301south.net>
2013-09-09 12:01:57 -0700

    Fixed format bug for totals in show.m

--- util/show.m
+++ util/show.m
@@ -219,6 +219,9 @@ for i=1:length(FN)
   else
     hdr_fmt{i}=['%-' num2str(floor(str2num(t_fmt{1}{2}))) 's'];
     width(i)=floor(str2num(t_fmt{1}{2}));
+    if ~ismember(t_fmt{1}{3}(1),{'c','s','v'})
+      accumfmt(i)='a';
+    end
   end
   if i==length(fmt) & i < length(FN)
     fmt{i+1}=fmt{i}; % continue last-supplied fmt
@@ -262,6 +265,7 @@ end
 if length(D)>1 & bitand(cont,1)==0
   % also add total row
   fprintf(fid,['%s' NEWLINE],'TOTAL');
+  keyboard
   sumtotal=struct2cell(accum(D,accumfmt));
   sumtotal=sumtotal(1:end-1);
   [mydat{accumfmt=='d'}]=deal(' ');
