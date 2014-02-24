502a3f031c8497f6d772807a2ae42c5fc0853ff4
Brandon Kuczenski <bkuczenski@bren.ucsb.edu>
2014-02-24 12:16:50 -0800

    Rewrite bisect_find to work properly.

--- util/bisect_find.m
+++ util/bisect_find.m
@@ -10,35 +10,40 @@ function ind=bisect_find(key,ref,argin);
 %  ind = current pointer, starts at 0
 %  d   = direction of next bisection (1 or -1)
 
-if length(ref)<4
+if length(ref)<10
   ind=find(strcmp(key,ref));
   if isempty(ind)
       [~,d]=sort([ref(:); key]);
-      ind=d(end)-1;
+      ind=find(d==length(d))-1;
   end
   return
 end
 debug=false;
 if nargin==3 
   debug=true; 
-  fprintf('%10s %10s %8s \n','siz','ind','diff(d)')
+  fprintf('%10s %10s %8s %s\n','siz','ind','diff(d)',key)
 end
-siz=length(ref)/2-0.5;ind=1;d=1;
-while siz>0.5
-  ind=max([ind+d*siz,1]);
-  siz=siz/2;
-  r=ref{round(ind)};
+
+L=length(ref);siz=L;ind=0;d=1;
+while 1
+  siz=ceil(siz/2);
+  ind=min([ind+d*siz,L]);
+  r=ref{ind};
   try 
     if ['a' key]==['a' r] break; end % 'a' to avoid '' == '' fail
   end
-  [~,d]=sort({r;key});
-  if debug fprintf('%10g %10g %8d\n',siz,ind,diff(d)); end
-  d=diff(d);
-  if siz<=0.5
-    ind=ind+1.01*d*siz; % this is to deal with small-number difficulties
+  [~,new_d]=sort({r;key});
+  new_d=diff(new_d);
+  if debug fprintf('%10g %10g %8d %s\n',siz,ind,new_d,r); end
+  % break when: siz==1 and d and new_d have opposite signs
+  if siz*d*new_d==-1 || (siz==1 & ind==L)
+      if new_d==-1
+          ind=ind-1;
+      end
+      break
   end
+  d=new_d;
 end
-ind=max([round(ind),1]);
-if debug fprintf('%10g %10g %8d final\n',siz,ind,diff(d)); end
+if debug fprintf('%10g %10g %8d %s final\n',siz,ind,d,ref{ind}); end
 
 
